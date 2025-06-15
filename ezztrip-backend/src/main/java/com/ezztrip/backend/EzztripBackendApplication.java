4c802f0f811bf18b77c15c00a6c5c48c5ba2f8b3
Lightening-00 <116797688+Lightening-00@users.noreply.github.com>
2025-06-15 16:37:23 +0530

    Updated Modularity

---
+++ ezztrip-backend/src/main/java/com/ezztrip/backend/EzztripBackendApplication.java
@@ -0,0 +1,14 @@
+package com.ezztrip.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EzzTripBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EzzTripBackendApplication.class, args);
+        System.out.println("Ezz-Trip Backend Application Started Successfully!");
+    }
+
+}
---
+++ ezztrip-backend/src/test/java/com/ezztrip/backend/EzztripBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ezztrip.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EzztripBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
